e87f0821c6659e6a84189f3bb6eb0d354f008dd4
print-aash <ashusingh88106283@gmail.com>
2025-10-18 01:23:41 +0530

    Add files via upload

---
+++ qr app/app.js
@@ -0,0 +1,32 @@
+//js File \\
+let input = document.querySelector(".user-input");
+let userInput = "";
+
+document.querySelector("#reset-btn").addEventListener("click", () => {
+  console.log("Reset Clicked !");
+  input.value = "";
+  img.remove();
+  buttonC.remove();
+  a.remove();
+});
+
+const btn = document.querySelector("#button").addEventListener("click", () => {
+  userInput = input.value;
+  if (!userInput) return alert("Please enter text");
+  const QR_URL = `http://api.qrserver.com/v1/create-qr-code/?data=${userInput}&size=100x100`;
+  fetch(QR_URL)
+    .then((res) => res.blob())
+    .then((blob) => {
+      img = document.createElement("img");
+      img.src = URL.createObjectURL(blob);
+      document.querySelector("body").appendChild(img);
+      a = document.createElement("a");
+      buttonC = document.createElement("button");
+      buttonC.innerText = "Downlode QR";
+      document.querySelector("body").appendChild(a).appendChild(buttonC);
+      a.href = `${img.src}`;
+    })
+    .catch((err) => {
+      console.log("Error", err);
+    });
+});
